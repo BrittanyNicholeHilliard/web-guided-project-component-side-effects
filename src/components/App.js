183012419b6e4be9a8b185c5477e848cef473bf1
timothy hoover <bnicholehilliard@gmail.com>
2022-04-20 22:04:06 -0700

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 // 👉 TASK 1 - import the axios lib from node_modules
-
-// 👉 TASK 2 - import the contants from constants/index.js
-
-import Details from './Details'
+import axios from 'axios';
+// 👉 TASK 2 - import the contents from constants/index.js
+import { BASE_URL, API_KEY } from '../constants'; 
+import Details from './Details';
 import Friend from './Friend';
 
 export default function App() {
   const [friends, setFriends] = useState([])
   const [currentFriendId, setCurrentFriendId] = useState(null)
+
+
 
   const openDetails = id => {
     setCurrentFriendId(id)
@@ -23,10 +25,19 @@ export default function App() {
   // The effect should consist of a call to the API using axios.
   // On success, set the array of friend objects from the API into state.
 
+useEffect(() => {
+  axios.get(`${BASE_URL}/friends?api_key=${API_KEY}`)
+  .then(res => {
+    setFriends(res.data);
+  }).catch(err => console.error(err))
+}, [])
+
   return (
     <div className='container'>
       <h1>Some of my friends:</h1>
-      {/* start by mapping over the friends array...*/}
+      {friends.length ? friends.map(fr => {
+        return <Friend key={fr.id} info={fr} open={openDetails}/>
+      }) : <p>Loudout Populating...</p>}
       {
         currentFriendId && <Details friendId={currentFriendId} close={closeDetails} />
       }
